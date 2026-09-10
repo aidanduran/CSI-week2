62afe04c9d7901c5e2dfd3f4abff705a37898d1b
Aidan Duran <aidan.duran007@gmail.com>
2026-09-10 15:05:05 -0400

    Simplified code as functions have not been taught yet.

--- Week2_AidanDuran.py
+++ Week2_AidanDuran.py
@@ -1,23 +1,11 @@
 hamburgers = 15
 hotdogs = 10
 
-def calculate(operation):
-    if operation == "addition":
-        return hamburgers + hotdogs
-    elif operation == "subtraction":
-        return hamburgers - hotdogs
-    elif operation == "multiplication":
-        return hamburgers * hotdogs
-    elif operation == "division":
-        return hamburgers / hotdogs
-    else:
-        return "Invalid operation"
-
-food_addition = calculate("addition")
+food_addition = hamburgers + hotdogs
 print(food_addition)
-food_subtraction = calculate("subtraction")
+food_subtraction = hamburgers - hotdogs
 print(food_subtraction)
-food_multiplication = calculate("multiplication")
+food_multiplication = hamburgers * hotdogs
 print(food_multiplication)
-food_division = calculate("division")
+food_division = hamburgers / hotdogs
 print(food_division)
